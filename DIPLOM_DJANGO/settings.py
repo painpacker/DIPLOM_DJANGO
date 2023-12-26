12d78a505ffcfdfc367df551579425eacf4de450
painpacker <109164927+painpacker@users.noreply.github.com>
2023-12-26 14:32:21 +0300

    Update settings.py
    ops

--- DIPLOM_DJANGO/settings.py
+++ DIPLOM_DJANGO/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o-_qu7c$(^9&@vaxah8kovp^w8!^7)xpdqrmal1qmsnpne_dpn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
